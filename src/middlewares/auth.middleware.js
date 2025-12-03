0fe042bb9f52b4e97e274447a485d952d9250e8b
Aditya Raj <adityarajmahule123.com>
2025-12-03 23:32:29 +0530

    implement JWT verification middleware for user authentication

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,2 +1,25 @@
 import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.startsWith("Bearer ");
+  if (!token) {
+    return next(new ApiError("Unauthorized", 401));
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-passsword -refreshToken -emailVerificationToken -emailVerificationExpiry" // Excluding sensitive fields
+    );
+    if (!user) {
+      return next(new ApiError("Invalid access token", 401));
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError("Invalid access token", 401);
+  }
+});
