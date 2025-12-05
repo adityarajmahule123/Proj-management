c8e6d19f378e6224ee925aad8f609ac03211270f
Aditya Raj <adityarajmahule123.com>
2025-12-06 01:26:07 +0530

    add forgot password request functionality

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,7 +2,11 @@ import { User } from "../models/user.models.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
-import { emailVerificationMailGenContent, sendEmail } from "../utils/mail.js";
+import {
+  emailVerificationMailGenContent,
+  forgotPasswordMailgenContent,
+  sendEmail,
+} from "../utils/mail.js";
 import jwt from "jsonwebtoken";
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -254,6 +258,31 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(404, "User with this email does not exists");
+  }
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.forgotPasswordToken = hashedToken;
+  user.forgotPasswordExpiry = tokenExpiry;
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Password reset request",
+    mailgenContent: forgotPasswordMailgenContent(
+      user.username,
+      `${process.env.FORGOT_PASSWORD_REDIRECT_URL}/${unHashedToken}`
+    ),
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Password reset email sent successfully"));
+});
 export {
   registerUser,
   login,
@@ -262,4 +291,5 @@ export {
   verifyEmail,
   resendEmailverification,
   refreshAccessToken,
+  forgotPasswordRequest,
 };
