4d882382f76d8d5cd0a11bd447aca748fd2bde10
Aditya Raj <adityarajmahule123.com>
2025-12-06 01:38:03 +0530

    add user registration functionality with email verification

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -283,6 +283,33 @@ const forgotPasswordRequest = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, null, "Password reset email sent successfully"));
 });
+
+const resetForgotPassword = asyncHandler(async (req, res) => {
+  const { resetToken } = req.params;
+  const { newPassword } = req.body;
+  let hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  const user = await User.findOneAndUpdate({
+    forgotPasswordToken: hashedToken,
+    forgotPasswordExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new ApiError(489, "Token is invalid or expired");
+  }
+  user.forgotPasswordExpiry = undefined;
+  user.forgotPasswordToken = undefined;
+  user.password = newPassword;
+
+  await user.save({ validateBeforeSave: false });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Password has been reset successfully"));
+});
+
 export {
   registerUser,
   login,
